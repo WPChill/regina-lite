d85f4c49d3c937cfe8c46b3e280c9121d619ccb6
Robciuc Ioan <robciucioan94@gmail.com>
2015-11-11 10:21:41 +0200

    Resolved #2.

--- functions.php
+++ functions.php
@@ -197,9 +197,9 @@ function regina_lite_scripts() {
 	wp_enqueue_script( 'regina-lite-jquery.bxslider.min', get_template_directory_uri() .'/layout/js/plugins/bxslider/jquery.min.js', array( 'jquery' ), '', true );
 	wp_enqueue_script( 'regina-lite-jquery.lazyload.min', get_template_directory_uri() . '/layout/js/plugins/lazyload/jquery.min.js', array( 'jquery' ), '', true );
 	wp_enqueue_script( 'regina-lite-jquery.waypoints.min', get_template_directory_uri() . '/layout/js/plugins/waypoints/jquery.min.js', array( 'jquery' ), '', true );
-	wp_enqueue_script( 'regina-lite-custom', get_template_directory_uri() . '/layout/js/custom.js', array( 'jquery' ), '', true );
 	wp_enqueue_script( 'regina-lite-navigation', get_template_directory_uri() . '/layout/js/plugins/navigation/navigation.js', array( 'jquery' ), '20120206', true );
 	wp_enqueue_script( 'regina-lite-skip-link-focus-fix', get_template_directory_uri() . '/layout/js/plugins/skip-link-focus-fix/skip-link-focus-fix.js', array( 'jquery' ), '20130115', true );
+	wp_enqueue_script( 'regina-lite-custom', get_template_directory_uri() . '/layout/js/custom.js', array( 'jquery' ), '', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
