1f306c1e71419f0b84037eedb823fc0427e65747
Giucu <giucu91@yahoo.com>
2017-07-20 13:44:15 +0300

    Fix custom header error.

--- sections/section-home-slider.php
+++ sections/section-home-slider.php
@@ -2,18 +2,25 @@
 
 $custom_header = get_custom_header();
 
-$image = wp_get_attachment_image_url( $custom_header->attachment_id, 'regina-lite-slider-image-sizes' );
+if ( ! isset( $custom_header->url ) || '' == $custom_header->url ) {
+	return;
+}
+
+$url = $custom_header->url;
 
 // get image meta by ID
-$image_meta = get_post_meta( $image_id );
-
+$image_alt = get_bloginfo( 'name' );
+if ( isset( $custom_header->attachment_i ) ) {
+	$image_meta = get_post_meta( $custom_header->attachment_id );
+	$image_alt = $image_meta['_wp_attachment_image_alt']['0'];
+}
 
 ?>
 
 <div id="home-slider" class="clear">
 	<ul class="clear">
 		<li>
-			<img src="<?php echo esc_url( get_custom_header()->url ); ?>" alt="<?php echo esc_attr( $image_meta['_wp_attachment_image_alt']['0'] ); ?>">
+			<img src="<?php echo esc_url( $url ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>">
 		</li>
 	</ul>
 	<div class="clear"></div>
